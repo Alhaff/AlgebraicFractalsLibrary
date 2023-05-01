2a91e22d532e87cdf501a7ad494e5f4022fb59c4
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-01 15:09:36 +0300

    Add project files.

---
+++ AlgebraicFractalBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicFractals
+{
+    public abstract class AlgebraicFractalBase
+    {
+        public Coord<double> TopLeft { get; set; }
+
+        public Coord<double> BottomRight { get; set; }
+
+        public AlgebraicFractalBase(Coord<double> topLeftCoord, Coord<double> bottomRightCoord)
+        {
+            TopLeft = topLeftCoord;
+            BottomRight = bottomRightCoord;
+        }
+
+        public abstract int FractalEquasion(double x, double y, double MaxIterations);
+
+        public void CreateFractalSimple(int[] image, int imageWidth, Coord<int> imageTopLeft, Coord<int> imageBottomRight, int MaxIterations)
+        {
+            double xScale = (BottomRight.X - TopLeft.X) / (imageBottomRight.X - imageTopLeft.X);
+            double yScale = (BottomRight.Y - TopLeft.Y) / (imageBottomRight.Y - imageTopLeft.Y);
+
+            for (int y = imageTopLeft.Y; y < imageBottomRight.Y; y++)
+            {
+                for (int x = imageTopLeft.X; x < imageBottomRight.X; x++)
+                {
+
+                    image[y * imageWidth + x] = FractalEquasion(x * xScale + TopLeft.X, y * yScale + TopLeft.Y, MaxIterations);
+            
+                 }
+            }
+        }
+    }
+}
---
+++ Coord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicFractals
+{
+    public struct Coord<T> where T : INumber<T>
+    {
+        public Coord(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+        public T X { get; init; }
+        public T Y { get; init; }
+    }
+}
