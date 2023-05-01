d47f4fb467e064a0e13fcd3b1f6d13e59354d931
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-01 15:21:53 +0300

    Add implementations of mandelbrotSet

---
+++ MandelbrotSet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicFractals
+{
+    public class MandelbrotSet : AlgebraicFractalBase
+    {
+        public MandelbrotSet() : base(new Coord<double>(-2,-1), new Coord<double>(1,1))
+        {
+        }
+
+        public override int FractalEquasion(double x, double y, double MaxIterations)
+        {
+            Complex C = new Complex(x,y);
+            Complex Z = new Complex(0,0);
+            int n = 0;
+            while (Z.Magnitude < 2.0 && n < MaxIterations)
+            {
+                Z = (Z * Z) + C;
+                n++;
+            }
+            return n;
+        }
+    }
+}
