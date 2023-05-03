5a168394865c29491efcfe15e92b00cfc19fac85
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-03 22:15:46 +0300

    Added functionality to support works in thread pool

--- AlgebraicFractalBase.cs
+++ AlgebraicFractalBase.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AlgebraicFractals
@@ -17,6 +18,8 @@ namespace AlgebraicFractals
             TopLeft = topLeftCoord;
             BottomRight = bottomRightCoord;
         }
+
+        #region BaseCreateFractal
         /// <summary>
         /// Must return iterations count from input coord x, y
         /// </summary>
@@ -60,6 +63,133 @@ namespace AlgebraicFractals
         /// <param name="MaxIterations"></param>
         public abstract void CreateFractalIntrinsics(int[] image, int imageWidth,
             Coord<int> imageTopLeft, Coord<int> imageBottomRight, int MaxIterations);
+        #endregion
+
+
+        #region ThreadWork
+
+        public int ThreadCount { get; set; } = 32;
+        public class FractalThreadContext
+        {
+            public FractalThreadContext(int[] image, int imageWidth,
+                Coord<double> fractalTL, Coord<double> fractalBR,
+                Coord<int> imageTL, Coord<int> imageBR,
+                double xScale, double yScale,int maxIter, ManualResetEvent done)
+            {
+                Image = image;
+                ImageWidth = imageWidth;
+                FractalTL = fractalTL;
+                FractalBR = fractalBR;
+                ImageTL = imageTL;
+                ImageBR = imageBR;
+                XScale = xScale;
+                YScale = yScale;
+                MaxIter = maxIter;
+                DoneEvent = done;
+            }
+
+            public int[] Image { get; init; }
+            public int ImageWidth { get; init; }
+            public Coord<double> FractalTL { get; init; }
+            public Coord<double> FractalBR { get; init; }
+            public Coord<int> ImageTL { get; init; }
+            public Coord<int> ImageBR { get; init; }
+            public double XScale { get; init; }
+            public double YScale { get; init; }
+            public int MaxIter { get; init; }
+            public ManualResetEvent DoneEvent { get; init; }
+        } 
+        private void CreateFractalSimpleInThread(object context)
+        {
+            var ctx =  context as FractalThreadContext;
+            int[] image = ctx.Image;
+            int imageWidth = ctx.ImageWidth;
+            Coord<double> fractalTopLeft = ctx.FractalTL;
+            Coord<double> fractalBottomRight = ctx.FractalBR;
+            Coord<int> imageTopLeft = ctx.ImageTL; 
+            Coord<int> imageBottomRight = ctx.ImageBR;
+            int MaxIterations = ctx.MaxIter;
+            double xScale = ctx.XScale;
+            double yScale = ctx.YScale;
+            for (int y = imageTopLeft.Y; y < imageBottomRight.Y; y++)
+            {
+                for (int x = imageTopLeft.X; x < imageBottomRight.X; x++)
+                {
+                    if(y + imageWidth + x < image.Length)
+                        image[y * imageWidth + x] = ColorINTFromIterationsAmount(
+                        FractalEquasion((x * xScale) + TopLeft.X, (y*yScale) + TopLeft.Y, MaxIterations));
+                }
+            }
+            ctx.DoneEvent.Set();
+        }
+
+        public void CreateFractalSimpleInThreadPool(int[] image, int imageWidth,
+            Coord<int> imageTopLeft, Coord<int> imageBottomRight, int MaxIterations)
+        {
+            int nSectionWidth = (int)((imageBottomRight.X - imageTopLeft.X) / (double)ThreadCount);
+            double dFractalWidth = (BottomRight.X - TopLeft.X) / (double)ThreadCount;
+            double xScale = (BottomRight.X - TopLeft.X) / (double)(imageBottomRight.X - imageTopLeft.X);
+            double yScale = (BottomRight.Y - TopLeft.Y) / (double)(imageBottomRight.Y - imageTopLeft.Y);
+            ManualResetEvent[] dones = new ManualResetEvent[ThreadCount];
+            for(int i =0; i < ThreadCount;i++)
+            {
+                dones[i] = new ManualResetEvent(false);
+                var FrBr = i == ThreadCount - 1 ? BottomRight.X : TopLeft.X + dFractalWidth * (i + 1);
+                var ImBr = i == ThreadCount - 1 ? imageBottomRight.X : imageTopLeft.X + nSectionWidth * (i + 1);
+                var context = new FractalThreadContext(
+                       image,
+                       imageWidth,
+                       new Coord<double>(TopLeft.X + dFractalWidth * i, TopLeft.Y),
+                       new Coord<double>(FrBr, BottomRight.Y),
+                       new Coord<int>(imageTopLeft.X + nSectionWidth * i, imageTopLeft.Y),
+                       new Coord<int>(ImBr, imageBottomRight.Y),
+                       xScale, yScale,
+                       MaxIterations,
+                       dones[i]);
+                ThreadPool.QueueUserWorkItem(CreateFractalSimpleInThread, context);
+            }
+          
+            foreach (var e in dones)
+                e.WaitOne();
+        }
+
+        protected abstract void CreateFractalIntrinsicsInTread(object context);
+
+        public void CreateFractalIntrinsicsThreadPool(int[] image, int imageWidth,
+            Coord<int> imageTopLeft, Coord<int> imageBottomRight, int MaxIterations)
+        {
+            int nSectionWidth = (int)((imageBottomRight.X - imageTopLeft.X) / (double)ThreadCount);
+            double dFractalWidth = (BottomRight.X - TopLeft.X) / (double)ThreadCount;
+            double xScale = (BottomRight.X - TopLeft.X) / (double)(imageBottomRight.X - imageTopLeft.X);
+            double yScale = (BottomRight.Y - TopLeft.Y) / (double)(imageBottomRight.Y - imageTopLeft.Y);
+            ManualResetEvent[] dones = new ManualResetEvent[ThreadCount];
+          
+            for (int i = 0; i < ThreadCount; i++)
+            {
+                dones[i] = new ManualResetEvent(false);
+                var FrBr = i == ThreadCount - 1 ? BottomRight.X : TopLeft.X + dFractalWidth * (i + 1);
+                var ImBr = i == ThreadCount - 1 ? imageBottomRight.X : imageTopLeft.X + nSectionWidth * (i + 1);
+                var context = new FractalThreadContext(
+                       image,
+                       imageWidth,
+                       new Coord<double>(TopLeft.X + dFractalWidth * i, TopLeft.Y),
+                       new Coord<double>(FrBr, BottomRight.Y),
+                       new Coord<int>(imageTopLeft.X + nSectionWidth * i, imageTopLeft.Y),
+                       new Coord<int>(ImBr, imageBottomRight.Y),
+                       xScale, yScale,
+                       MaxIterations,
+                       dones[i]);
+                ThreadPool.QueueUserWorkItem(CreateFractalIntrinsicsInTread, context);
+            }
+            foreach (var e in dones)
+                e.WaitOne();
+        }
+        #endregion
+
+
+
+
+        #region Color
         public static int ColorINTFromIterationsAmount(double n, double alpha = 0.1d)
         {
             double red = 0.5d * Math.Sin(alpha * n) + 0.5d;
@@ -74,5 +204,6 @@ namespace AlgebraicFractals
             double blue = 0.5d * Math.Sin(alpha * n + 4.188d) + 0.5d;
             return ((int)(red * 255) , (int)(green * 255), (int)(blue * 255));
         }
+        #endregion
     }
 }
