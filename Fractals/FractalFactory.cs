76e28e9752ec95daa4fc5fe3af5d046b8b2df1fb
alhaffthemage@gmail.com <alhaffthemage@gmail.com>
2023-05-13 23:40:02 +0300

    Added new class FractalFactory

---
+++ Fractals/FractalFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraicFractals.Fractals
+{
+    public static class FractalFactory
+    {
+        public static AlgebraicFractal CreateFractal(ExistingFractals existingFractals, params object?[]? arguments)
+        {
+            var str = "AlgebraicFractals.Fractals." + existingFractals.ToString();
+            Type type = Type.GetType(str);
+            if (type != null) return (AlgebraicFractal)Activator.CreateInstance(type, arguments);
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = asm.GetType(str);
+                if (type != null) return (AlgebraicFractal)Activator.CreateInstance(type, arguments);
+            }
+            throw new TypeUnloadedException($"Cannot find type {str}");
+
+        }
+    }
+}
